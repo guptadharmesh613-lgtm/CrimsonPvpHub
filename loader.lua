cbe3f179e0450f12ad5605c1ad6dee1871406f1e
guptadharmesh613-lgtm <guptadharmesh613@gmail.com>
2026-04-03 10:25:00 +0530

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,9 +1,10 @@
- local R=loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Rayfield/main/source"))()
+local R=loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Rayfield/main/source"))()
 local W=R:CreateWindow({Name="Crimson PvP Hub 💜",LoadingTitle="Crimson PvP Hub",LoadingSubtitle="Indra Theme"})
 local T=W:CreateTab("Main",4483362458)
+
 T:CreateButton({
-Name="Load Script",
+Name="Load Crimson",
 Callback=function()
-loadstring(game:HttpGet("YOUR MAIN SCRIPT LINK"))()
+print("Crimson Loaded")
 end
 })
